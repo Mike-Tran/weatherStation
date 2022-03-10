5ea8fece8892c98f14207862f310377faaa3f2bd
Frank Lin <franklin88@gmail.com>
2022-03-10 08:12:48 -0800

    check it out

--- README.md
+++ README.md
@@ -4,51 +4,26 @@ This is a single page weather app that shows the current weather for the week fo
 
 ## User Stories:
 
-As a user I want to be able to search for any location via zip code <br />
+As a user I want to be able to search for any location via zip code
 So that I can easily and quickly choose see the weather for various locations.
 
-As a user I want to see pictures of the approximate weather forecast (sun/rain/clouds) <br />
+As a user I want to see pictures of the approximate weather forecast (sun/rain/clouds)
 So that I can make quick estimations of the weather without having to read actual numbers.
 
-As a user I want to see detailed weather information for a location <br />
+As a user I want to see detailed weather information for a location
 So that I can prepare and dress appropriately for outdoor conditions.
 
-As a user I want all information to be accessed at one glance <br />
+As a user I want all information to be accessed at one glance
 So that I do not need to click through multiple buttons to view one piece of weather data.
 
 ## Wireframe(s):
 
 ![alt text](https://github.com/Mike-Tran/weatherStation/blob/main/frank-wireframe-screenshot.png)
 
-## API:
-
-OpenWeather: https://openweathermap.org/api
-
-
-## Goals:
-
-Single page application <br />
-Event Listener: DocumentContentLoaded <br />
-Today’s date <br />
-The current temperature <br />
-The weeks temperature <br />
-Detailed information on today's weather <br />
-Humidity <br />
-Pressure <br />
-Wind speed <br />
-Highs and lows 
-Etc. <br />
-Input form to query a US zip code and area <br />
-Event Listener: Form submission <br />
-Celsius to Fahrenheit conversion <br />
-Event Listener: Button click (button indicates which system is used) <br />
-
-Our event listeners will be the following: <br />
-Form submission <br />
-documentContentLoaded <br />
-Click: when a user clicks on the C/F button it will convert Celsuis to Fahrenheit, and vice versa <br />
-
 ## Stretch Goals:
 
 Have location cards for previuosly searched zip codes (This would be a POST request)
 
+## API:
+
+OpenWeather: https://openweathermap.org/api
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-const apiKey = '329cbaeef2629343499bd641a0fa58d0';
+const apiKey = '29d46196468dda5ecc5548f448aa899c';
 const lat = 47.73424240534853;
 const lon = -122.30687432498033;
-const weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=29d46196468dda5ecc5548f448aa899c`;
 const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let isFarenheit = true;
 let currentTempInKelvin;
+let currentFeelsLike;
+let currentHumidity;
+let currentUvi;
+let currentWindspeed;
+let currentPrecipitation;
 
 // Create object containing relavent icons for weather forcast 'light rain' etc should be 9  options
 // https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
@@ -14,22 +19,60 @@ const weatherIcons = {
 
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
     const currentTemp = document.querySelector('#currentTemp');
     const tempFormatBtn = document.querySelector('#tempFormatBtn');
-
+    const dayoneTemp = document.querySelector('#dayoneTemp');
+    const daytwoTemp = document.querySelector('#daytwoTemp');
+    const daythreeTemp = document.querySelector('#daythreeTemp');
+    const dayfourTemp = document.querySelector('#dayfourTemp');
+    const dayfiveTemp = document.querySelector('#dayfiveTemp');
+    const feelsLike = document.querySelector('#feelslike');
+    
+    
     init();
 
     tempFormatBtn.addEventListener('click', function() {
         if (isFarenheit) {
             currentTemp.textContent = kelvinToCelcius(currentTempInKelvin) + '°';
+           
+            dayoneTemp.textContent = kelvinToCelcius(dayOneTemp) + '°';
+            
+            daytwoTemp.innerHTML = kelvinToCelcius(dayTwoTemp) + '°';
+            
+            daythreeTemp.innerHTML = kelvinToCelcius(dayThreeTemp) + '°';
+            
+            dayfourTemp.innerHTML = kelvinToCelcius(dayFourTemp) + '°';
+            
+            dayfiveTemp.innerHTML = kelvinToCelcius(dayFiveTemp) + '°';
+            
+            feelsLike.innerHTML = kelvinToCelcius(currentFeelsLike) + '°';
             isFarenheit = !isFarenheit;
         } else {
-            currentTemp.textContent = kelvinToFarenheit(currentTempInKelvin) + '°';
+            currentTemp.innerHTML = kelvinToFarenheit(currentTempInKelvin) + '°';
+            
+            dayoneTemp.innerHTML = kelvinToFarenheit(dayOneTemp) + '°';
+            
+            daytwoTemp.innerHTML = kelvinToFarenheit(dayTwoTemp) + '°';
+            
+            daythreeTemp.innerHTML = kelvinToFarenheit(dayThreeTemp) + '°';
+            
+            dayfourTemp.innerHTML = kelvinToFarenheit(dayFourTemp) + '°';
+            
+            dayfiveTemp.innerHTML= kelvinToFarenheit(dayFiveTemp) + '°';
+            
+            feelsLike.innerHTML = kelvinToFarenheit(currentFeelsLike) + '°';
             isFarenheit = !isFarenheit;
         }
     });
+   
 });
+
+    const precipitation = document.querySelector('#precipitation');
+    const humidity = document.querySelector('#humidity');
+    const windspeed = document.querySelector('#windspeed');
+    const uvindex = document.querySelector('#uvindex');
 
 
 function init() {
@@ -42,29 +85,89 @@ function init() {
 function renderWeatherCards(weeksWeather) {
     console.log(weeksWeather);
     currentTempInKelvin = weeksWeather.current.temp;
-    currentTemp.textContent = kelvinToFarenheit(weeksWeather.current.temp) + '°';
+    currentTemp.textContent = kelvinToFarenheit(currentTempInKelvin) + '°';
     getDate();
+    currentFeelsLike = kelvinToFarenheit(weeksWeather.current.feels_like);
+    currentPrecipitation = weeksWeather.minutely[0].precipitation
+    currentHumidity = weeksWeather.current.humidity;
+    currentWindSpeed = weeksWeather.current.wind_speed;
+    currentUvIndex = weeksWeather.current.uvi;
 
-    for(let i = 0; i < 5; i++) {
-        renderSingleWeatherCard(weeksWeather.daily[i]);
-    }
-
+    dayOneTemp = kelvinToFarenheit(weeksWeather.daily[0].temp.day);
+    dayTwoTemp = kelvinToFarenheit(weeksWeather.daily[1].temp.day);
+    dayThreeTemp = kelvinToFarenheit(weeksWeather.daily[2].temp.day);
+    dayFourTemp = kelvinToFarenheit(weeksWeather.daily[3].temp.day);
+    dayFiveTemp = kelvinToFarenheit(weeksWeather.daily[4].temp.day);
+    renderDetailedData()
+    // renderSingleWeatherCard()
+   //  
 }
 
 function renderSingleWeatherCard(weatherInfo) {
-    const weekViewContainer = document.querySelector('.weekViewContainer');
-    const card = document.createElement('div');
-    card.className = 'dayCard';
+    let weekViewContainer = document.querySelector('.weekViewContainer');
+    weekViewContainer.innerHTML = `
+    <div class="futureCard">
+    <i class="fa-8x bi bi-cloud-sun"></i>
+    <div class="card-body">
+      <h3 class="card-title" id="dayoneDate">Tomorrow</h3>
+      <p class="card-text">${dayOneTemp}°</p>
+      <h1 id="dayoneTemp"></h1>
+    </div>
+  </div>
 
-    // link weather forecast for this day to icon and append to card
-    const weatherIcon = document.createElement('i');
-    weatherIcon.className = "fa-8x bi bi-cloud-sun";
+  <div class="futureCard">
+    <i class="fa-8x bi bi-cloud-rain"></i>
+    <div class="card-body">
+      <h3 class="card-title" id="daytwoDate">Day 2</h3>
+      <p class="card-text">${dayTwoTemp}°</p>
+      <h1 id="daytwoTemp"></h1>
+    </div>
+  </div>
 
-    const dayTemp = document.createElement('h2');
-    dayTemp.className = 'center';
-    dayTemp.textContent = `${kelvinToFarenheit(weatherInfo.temp.day)}°`;
-    card.append(weatherIcon, dayTemp);
-    weekViewContainer.append(card);
+  <div class="futureCard">
+    <i class="fa-8x bi bi-cloud-hail"></i>
+    <div class="card-body">
+      <h3 class="card-title" id="daythreeDate">Day 3</h3>
+      <p class="card-text">${dayThreeTemp}°</p>
+      <h1 id="daythreeTemp"></h1>
+    </div>
+  </div>
+
+  <div class="futureCard">
+    <i class="fa-8x bi bi-cloud-sun"></i>
+    <div class="card-body">
+      <h3 class="card-title" id="dayfourDate">Day 4</h3>
+      <p class="card-text">${dayFourTemp}°</p>
+      <h1 id="dayfourTemp"></h1>
+    </div>
+  </div>
+
+  <div class="futureCard">
+    <i class="fa-8x bi bi-cloud-hail"></i>
+    <div class="card-body">
+      <h3 class="card-title" id="dayfiveDate">Day 5</h3>
+      <p class="card-text">${dayFiveTemp}°</p>
+      <h1 id="dayfiveTemp"></h1>
+    </div>
+  </div>`
+}
+
+function renderDetailedData(data) {
+    let detailRow = document.querySelector('.detailContainer')
+    detailRow.innerHTML = `
+    <div class="detail">
+    <div class="card-body">
+      <h3 class="card-title">Details</h3>
+      <p class="card-text" id="feelslike">Feels Like ${currentFeelsLike}°</p>
+      <p class="card-text" id="precipitation">Precipitation ${currentPrecipitation}%</p>
+      <p class="card-text" id="humidity">Humidity ${currentHumidity}%</p>
+      <p class="card-text" id="windspeed">Wind Speed ${currentWindSpeed} mph</p>
+      <p class="card-text" id="uvindex">UV Index ${currentUvIndex}</p>
+      <h1 id="details"></h1>
+
+  </div>   `
+
+  
 }
 
 function getDate() {
@@ -83,4 +186,6 @@ function kelvinToFarenheit(tempInKelvin) {
 function kelvinToCelcius(tempInKelvin) {
     return Math.round(tempInKelvin-273.15);
 }
+
+
 
